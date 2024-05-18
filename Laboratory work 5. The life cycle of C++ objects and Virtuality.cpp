6f40c825debf8ce4287ccd92f272171f36febfdc
vplotnikov16 <vplotnikov16@yandex.ru>
2024-05-18 22:11:59 +0500

    Реализация проверки на принадлежность некоторому классу, безопасного приведения типов вручную и с помощью dynamic_cast

--- Laboratory work 5. The life cycle of C++ objects and Virtuality.cpp
+++ Laboratory work 5. The life cycle of C++ objects and Virtuality.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 class Shape {
 public:
+    virtual bool isA(const string& s) {
+        return s == "Shape";
+    }
     virtual void area() = 0;
     void show() {
         printf("I am shape or its heir \n");
@@ -13,33 +16,69 @@ public:
 
 class Triangle : public Shape {
 public:
+    bool isA(const string& s) {
+        return (s == "Triangle") || Shape::isA(s);
+    }
     void area() {
         printf("S=0.5*a*h \n");
+    }
+    void triangleMethod() {
+        printf("triangleMethod \n");
+    }
+};
+
+class RightTriangle : public Triangle {
+public:
+    bool isA(const string& s) {
+        return (s == "RightTriangle") || Triangle::isA(s);
     }
 };
 
 class Rectangle : public Shape {
 public:
+    bool isA(const string& s) {
+        return (s == "Rectangle") || Shape::isA(s);
+    }
     void area() {
         printf("S=a*b \n");
     }
     void show() {
         printf("I am rectangle \n");
     }
+    void rectangleMethod() {
+        printf("rectangleMethod \n");
+    }
 };
 
 int main()
 {
-    Shape* s_tr = new Triangle();
-    Shape* s_rect = new Rectangle();
-    Triangle* tr = new Triangle();
-    Rectangle* rect = new Rectangle();
-    s_tr->area();  // перекрываемый метод
-    s_rect->area(); // перекрываемый метод
-    tr->area(); // перекрываемый метод
-    rect->area(); // перекрываемый метод
-    s_tr->show(); // наследуемый метод
-    s_rect->show(); // наследуемый метод
-    tr->show(); // наследуемый метод
-    rect->show(); // перекрываемый метод
+    srand(static_cast<unsigned int>(time(0)));
+
+    const int size = 10;
+    Shape* shapes[size];
+    for (int i = 0; i < size; i++) {
+        int choice = rand();
+        if (choice % 3 == 0) {
+            shapes[i] = new Triangle();
+        }
+        else if (choice % 3 == 1) {
+            shapes[i] = new RightTriangle();
+        }
+        else if (choice % 3 == 2) {
+            shapes[i] = new Rectangle();
+        }
+    }
+
+    // Безопасное приведение типов вручную
+    for (int i = 0; i < size; i++) {
+        if (shapes[i]->isA("Triangle")) {
+            static_cast<Triangle*>(shapes[i])->triangleMethod();
+        }
+    }
+    printf("\n");
+    // Безопасное приведение типов с использованием dynamic_cast
+    for (int i = 0; i < size; i++) {
+        Triangle* t = dynamic_cast<Triangle*>(shapes[i]);
+        if (t != nullptr) t->triangleMethod();
+    }
 }
