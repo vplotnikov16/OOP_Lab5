4c414ea4a810636ef52f1ee88a03ffa1492e6d3b
vplotnikov16 <vplotnikov16@yandex.ru>
2024-05-18 21:20:41 +0500

    Определение перекрываемых и виртуальных методов. Демонстрация вызовов перекрываемых и наследуемых методов, вызов виртуальных методом (в т.ч. деструкторов)

--- Laboratory work 5. The life cycle of C++ objects and Virtuality.cpp
+++ Laboratory work 5. The life cycle of C++ objects and Virtuality.cpp
@@ -6,21 +6,40 @@ using namespace std;
 class Shape {
 public:
     virtual void area() = 0;
+    void show() {
+        printf("I am shape or its heir \n");
+    }
 };
 
 class Triangle : public Shape {
+public:
     void area() {
         printf("S=0.5*a*h \n");
     }
 };
 
 class Rectangle : public Shape {
+public:
     void area() {
         printf("S=a*b \n");
+    }
+    void show() {
+        printf("I am rectangle \n");
     }
 };
 
 int main()
 {
-    
+    Shape* s_tr = new Triangle();
+    Shape* s_rect = new Rectangle();
+    Triangle* tr = new Triangle();
+    Rectangle* rect = new Rectangle();
+    s_tr->area();  // перекрываемый метод
+    s_rect->area(); // перекрываемый метод
+    tr->area(); // перекрываемый метод
+    rect->area(); // перекрываемый метод
+    s_tr->show(); // наследуемый метод
+    s_rect->show(); // наследуемый метод
+    tr->show(); // наследуемый метод
+    rect->show(); // перекрываемый метод
 }
