15760f8b447052f03c7b7e0c93bdfa4e1095ddd1
vplotnikov16 <vplotnikov16@yandex.ru>
2024-05-18 21:08:29 +0500

    Добавьте файлы проекта.

---
+++ Laboratory work 5. The life cycle of C++ objects and Virtuality.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class Shape {
+public:
+    virtual void area() = 0;
+};
+
+class Triangle : public Shape {
+    void area() {
+        printf("S=0.5*a*h \n");
+    }
+};
+
+class Rectangle : public Shape {
+    void area() {
+        printf("S=a*b \n");
+    }
+};
+
+int main()
+{
+    
+}
